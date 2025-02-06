1322144c7f7d26885788b422dbc6069ab8858678
Valter Yde Daugberg <valteryde@hotmail.com>
2025-02-06 19:29:51 +0100

    Tilføjede animationer

--- src/pylak/animation.py
+++ src/pylak/animation.py
@@ -15,23 +15,159 @@ from PIL import Image
 
 class Animations:
 
-    def __init__(self, texturePath:str, rows:int, cols:int, period=0.1):
+    
+    def __addAnimationState__(self, 
+                              fullSpriteSheet, 
+                              size, 
+                              y0, 
+                              y1, 
+                              cols, 
+                              period, 
+                              rowHeight,
+                              flipx=False,
+                              flipy=False
+                              ):
+        
+        # chop images into diffrent textures
+        spriteSheet = fullSpriteSheet.crop([0, y0, fullSpriteSheet.width, y1])
+        rawImage = spriteSheet.tobytes()  # tostring is deprecated
+        spriteSheet = pgl.image.ImageData(spriteSheet.width, spriteSheet.height, 'RGBA', rawImage, pitch=-spriteSheet.width * 4)
 
-        sprite_sheet = pgl.resource.image(texturePath)
-        self.animations = []
+        # load image into image grid and add animation
+        imageGrid = pgl.image.ImageGrid(spriteSheet, rows=1, columns=cols)
+        
+        animation = imageGrid.get_animation(period=period)
+        
+        for frame in animation.frames:
+            texture = frame.image.get_texture()
+            frame.image = texture.get_transform(flipx, flipy)
 
-        for _ in range(rows):
-            
-            image_grid = pgl.image.ImageGrid(sprite_sheet, rows=1, columns=cols)
-            animation = image_grid.get_animation(period=period)
-            self.animations.append( pgl.sprite.Sprite(img=animation) )
+        sprite = pgl.sprite.Sprite(img=animation)
 
-        self._currentAnimationIndex = 0
+        sprite.scale = size/rowHeight
+
+        return sprite
 
 
-    def switch(self, index:int):
-        self._currentAnimationIndex = index
+    def __init__(self, texturePath:str, rows:int, cols:int, period=1/15, size=100):
+
+        self.size = size
+
+        fullSpriteSheet = Image.open(texturePath)
+        rowHeight = fullSpriteSheet.height / rows
+        
+        self.cols = cols
+        self.rows = rows
+
+        self.animations = {
+            "default":[],
+            "flipx":[],
+            "flipy":[]
+        }
+
+        self._forceNext = None
+        self._forceNextActive = False
+
+        for i in range(rows):
+
+            self.animations["default"].append(self.__addAnimationState__(
+                fullSpriteSheet, 
+                size,
+                rowHeight*i, 
+                rowHeight*(i+1), 
+                cols, 
+                period, 
+                rowHeight
+            ))
+
+        for i in range(rows):
+
+            self.animations["flipx"].append(self.__addAnimationState__(
+                fullSpriteSheet, 
+                size,
+                rowHeight*i, 
+                rowHeight*(i+1), 
+                cols, 
+                period, 
+                rowHeight,
+                flipx=True
+            ))
+        
+        for i in range(rows):
+
+            self.animations["flipy"].append(self.__addAnimationState__(
+                fullSpriteSheet, 
+                size,
+                rowHeight*i, 
+                rowHeight*(i+1), 
+                cols, 
+                period, 
+                rowHeight,
+                flipy=True
+            ))
+
+        self._currentAnimation = [0, 'default']
+        self._nextAnimationIndex = 0
+        self._reachedEndOfCycle = False
+
+
+    def switch(self, index:int, flipx:bool=False, flipy:bool=False):
+        """
+        Bruges til states
+        """
+        
+        if flipx and flipy:
+            raise NotImplemented('Both flips can not happened at once')
+
+        state = 'default'
+        if flipx: state = 'flipx'
+        if flipy: state = 'flipy'
+
+        self._nextAnimationIndex = index
+        self._currentAnimation[1] = state
+
+
+    def do(self, index:int, flipx:bool=False, flipy:bool=False):
+        
+        self.switch(index, flipx, flipy)
+
+        self._forceNext = self._nextAnimationIndex
+
+
+    def setPos(self, x, y):
+        self.x = x
+        self.y = y
 
 
     def draw(self):
-        self.animations[self._currentAnimationIndex].draw()
+        
+        index, state = self._currentAnimation
+
+        sprite:pgl.sprite.Sprite = self.animations[state][index]
+        sprite.x = self.x + (state == "flipx") * self.size
+        sprite.y = self.y + (state == "flipy") * self.size
+
+        # denne burde kun køre en gang per cyklus men bliver kørt flere gange 
+        # hver gang der tegnes og man er på det sidste frame
+        if sprite.frame_index == self.cols - 1 and not self._reachedEndOfCycle:
+            
+            # hvis der er en ny forced animation som ikke er igang
+            if self._forceNext and not self._forceNextActive:
+                # så skal den sættes på nu, da animations cyklussen er klar 
+                self._forceNextActive = True
+                self._currentAnimation[0] = self._forceNext
+            
+            # hvis den forced animation er der og har været igang
+            elif self._forceNext and self._forceNextActive:
+                # så skal den sættes fjerens nu da animationen er done
+                self._forceNext = None
+                self._forceNextActive = False
+                self._currentAnimation[0] = self._nextAnimationIndex
+
+            # hvis der ingen forced er
+            else:
+                self._currentAnimation[0] = self._nextAnimationIndex
+            
+        self._reachedEndOfCycle = sprite.frame_index == self.cols - 1
+
+        sprite.draw()
--- tests/animation.py
+++ tests/animation.py
@@ -2,22 +2,52 @@
 # måske også lave en map class som holder styr på mappet
 
 import test
-from pylak import Engine, Animations
+from pylak import Engine, Animations, Rectangle
 
 engine = Engine()
 
 class Scene:
 
     def setup(self):
-        self.animations = Animations('Tiny_Swords/Factions/Knights/Troops/Warrior/Purple/Warrior_Purple.png', 8, 6)
-    
-    def update(self):
+        self.animations = Animations('tests/Tiny_Swords/Factions/Knights/Troops/Warrior/Purple/Warrior_Purple.png', 8, 6, size=200)
         
-        if engine.isKeyPressed('1'):
-            self.animations.switch()
+        self.rect = Rectangle(0, 0, 200, 200)
+
+        self.x = 0 
+        self.y = 0
+
+        self.velocity = 5
+
+
+    def update(self):
+        self.animations.setPos(self.x, self.y)
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.animations.switch(0)
+
+        if engine.isKeyPressed('a'):
+            self.x -= self.velocity
+            self.animations.switch(1, flipx=True)
+        if engine.isKeyPressed('d'):
+            self.x += self.velocity
+            self.animations.switch(1)
+        if engine.isKeyPressed('w'):
+            self.y += self.velocity
+            self.animations.switch(1)
+        if engine.isKeyPressed('s'):
+            self.y -= self.velocity
+            self.animations.switch(1)
+
+        if engine.isKeyPressed('e'):
+            self.animations.do(2)
+        
+        if engine.isKeyPressed('f'):
+            self.animations.do(3)
 
     
     def draw(self):
+        self.rect.draw()
         self.animations.draw()
 
 
